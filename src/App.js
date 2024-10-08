14a034006d753ae1fa13480f7654f772aaf9bfdf
asmitaneupane <neupane.ashmita2056@gmail.com>
2024-10-08 14:35:59 +0545

    dev: render welcome component if user

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from "./images/google-signin-button.png";
 import "./App.css";
 import { auth } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import NavBar from "./components/NavBar";
+import Welcome from "./components/Welcome";
 
 function App() {
   const [user] = useAuthState(auth);
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      {user && <Welcome />}
     </div>
   );
 }
