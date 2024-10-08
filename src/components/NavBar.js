42fae07d26774590e988269b5af334011c4183f3
asmitaneupane <neupane.ashmita2056@gmail.com>
2024-10-08 14:11:38 +0545

    modify signin and signout functions using firebase auth

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import GoogleSignin from "../images/Google Image.png";
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 
 const NavBar = () => {
-  const [user, setUser] = useState(false);
+  const [user] = useAuthState(auth);
   const googleSignIn = () => {
-    setUser(true);
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
   };
   const signOut = () => {
-    setUser(false);
+    auth.signOut();
   };
   return (
     <nav className="nav-bar">
